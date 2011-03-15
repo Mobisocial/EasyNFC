f67f8eb46ed9f21f2e872eca48b1e9603f75b582
Ben Dodson <bjdodson@handdog.(none)>
2011-03-14 17:42:15 -0700

    adding apidocs

---
+++ apidocs/reference/lists.js
@@ -0,0 +1,12 @@
+var DATA = [
+      { id:0, label:"mobisocial.nfc", link:"mobisocial/nfc/package-summary.html", type:"package" },
+      { id:1, label:"mobisocial.nfc.Nfc", link:"mobisocial/nfc/Nfc.html", type:"class" },
+      { id:2, label:"mobisocial.nfc.Nfc.ConnectionHandover", link:"mobisocial/nfc/Nfc.ConnectionHandover.html", type:"class" },
+      { id:3, label:"mobisocial.nfc.Nfc.NdefBluetoothPushHandover", link:"mobisocial/nfc/Nfc.NdefBluetoothPushHandover.html", type:"class" },
+      { id:4, label:"mobisocial.nfc.Nfc.NdefFactory", link:"mobisocial/nfc/Nfc.NdefFactory.html", type:"class" },
+      { id:5, label:"mobisocial.nfc.Nfc.NdefProxy", link:"mobisocial/nfc/Nfc.NdefProxy.html", type:"class" },
+      { id:6, label:"mobisocial.nfc.Nfc.NdefTcpPushHandover", link:"mobisocial/nfc/Nfc.NdefTcpPushHandover.html", type:"class" },
+      { id:7, label:"mobisocial.nfc.Nfc.OnTagReadListener", link:"mobisocial/nfc/Nfc.OnTagReadListener.html", type:"class" },
+      { id:8, label:"mobisocial.nfc.Nfc.OnTagWriteListener", link:"mobisocial/nfc/Nfc.OnTagWriteListener.html", type:"class" }
+
+    ];
