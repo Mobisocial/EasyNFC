2bcb964dd24c2182b39e0c801327b5332632d841
Ben Dodson <bjdodson@handdog.(none)>
2011-03-17 18:01:24 -0700

    Updated apidocs

--- apidocs/assets/navtree_data.js
+++ apidocs/assets/navtree_data.js
@@ -1,6 +1,9 @@
 var NAVTREE_DATA =
-[ [ "mobisocial.nfc", "mobisocial/nfc/package-summary.html", [ [ "Description", "mobisocial/nfc/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "Nfc.ConnectionHandover", "mobisocial/nfc/Nfc.ConnectionHandover.html", null, "" ], [ "Nfc.NdefHandler", "mobisocial/nfc/Nfc.NdefHandler.html", null, "" ], [ "Nfc.NdefProxy", "mobisocial/nfc/Nfc.NdefProxy.html", null, "" ], [ "Nfc.OnTagWriteListener", "mobisocial/nfc/Nfc.OnTagWriteListener.html", null, "" ] ]
-, "" ], [ "Classes", null, [ [ "Nfc", "mobisocial/nfc/Nfc.html", null, "" ], [ "Nfc.NdefBluetoothPushHandover", "mobisocial/nfc/Nfc.NdefBluetoothPushHandover.html", null, "" ], [ "Nfc.NdefFactory", "mobisocial/nfc/Nfc.NdefFactory.html", null, "" ], [ "Nfc.NdefTcpPushHandover", "mobisocial/nfc/Nfc.NdefTcpPushHandover.html", null, "" ] ]
+[ [ "mobisocial.nfc", "mobisocial/nfc/package-summary.html", [ [ "Description", "mobisocial/nfc/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "Nfc.ConnectionHandover", "mobisocial/nfc/Nfc.ConnectionHandover.html", null, "" ], [ "Nfc.NdefHandler", "mobisocial/nfc/Nfc.NdefHandler.html", null, "" ], [ "Nfc.NfcInterface", "mobisocial/nfc/Nfc.NfcInterface.html", null, "" ], [ "Nfc.OnTagWriteListener", "mobisocial/nfc/Nfc.OnTagWriteListener.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Nfc", "mobisocial/nfc/Nfc.html", null, "" ], [ "Nfc.ConnectionHandoverManager", "mobisocial/nfc/Nfc.ConnectionHandoverManager.html", null, "" ], [ "Nfc.NdefBluetoothPushHandover", "mobisocial/nfc/Nfc.NdefBluetoothPushHandover.html", null, "" ], [ "Nfc.NdefFactory", "mobisocial/nfc/Nfc.NdefFactory.html", null, "" ], [ "Nfc.NdefTcpPushHandover", "mobisocial/nfc/Nfc.NdefTcpPushHandover.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "mobisocial.nfc.addon", "mobisocial/nfc/addon/package-summary.html", [ [ "Description", "mobisocial/nfc/addon/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "BluetoothConnector.OnConnectedListener", "mobisocial/nfc/addon/BluetoothConnector.OnConnectedListener.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "BluetoothConnector", "mobisocial/nfc/addon/BluetoothConnector.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
 
--- apidocs/reference/lists.js
+++ apidocs/reference/lists.js
@@ -2,11 +2,15 @@ var DATA = [
       { id:0, label:"mobisocial.nfc", link:"mobisocial/nfc/package-summary.html", type:"package" },
       { id:1, label:"mobisocial.nfc.Nfc", link:"mobisocial/nfc/Nfc.html", type:"class" },
       { id:2, label:"mobisocial.nfc.Nfc.ConnectionHandover", link:"mobisocial/nfc/Nfc.ConnectionHandover.html", type:"class" },
-      { id:3, label:"mobisocial.nfc.Nfc.NdefBluetoothPushHandover", link:"mobisocial/nfc/Nfc.NdefBluetoothPushHandover.html", type:"class" },
-      { id:4, label:"mobisocial.nfc.Nfc.NdefFactory", link:"mobisocial/nfc/Nfc.NdefFactory.html", type:"class" },
-      { id:5, label:"mobisocial.nfc.Nfc.NdefHandler", link:"mobisocial/nfc/Nfc.NdefHandler.html", type:"class" },
-      { id:6, label:"mobisocial.nfc.Nfc.NdefProxy", link:"mobisocial/nfc/Nfc.NdefProxy.html", type:"class" },
+      { id:3, label:"mobisocial.nfc.Nfc.ConnectionHandoverManager", link:"mobisocial/nfc/Nfc.ConnectionHandoverManager.html", type:"class" },
+      { id:4, label:"mobisocial.nfc.Nfc.NdefBluetoothPushHandover", link:"mobisocial/nfc/Nfc.NdefBluetoothPushHandover.html", type:"class" },
+      { id:5, label:"mobisocial.nfc.Nfc.NdefFactory", link:"mobisocial/nfc/Nfc.NdefFactory.html", type:"class" },
+      { id:6, label:"mobisocial.nfc.Nfc.NdefHandler", link:"mobisocial/nfc/Nfc.NdefHandler.html", type:"class" },
       { id:7, label:"mobisocial.nfc.Nfc.NdefTcpPushHandover", link:"mobisocial/nfc/Nfc.NdefTcpPushHandover.html", type:"class" },
-      { id:8, label:"mobisocial.nfc.Nfc.OnTagWriteListener", link:"mobisocial/nfc/Nfc.OnTagWriteListener.html", type:"class" }
+      { id:8, label:"mobisocial.nfc.Nfc.NfcInterface", link:"mobisocial/nfc/Nfc.NfcInterface.html", type:"class" },
+      { id:9, label:"mobisocial.nfc.Nfc.OnTagWriteListener", link:"mobisocial/nfc/Nfc.OnTagWriteListener.html", type:"class" },
+      { id:10, label:"mobisocial.nfc.addon", link:"mobisocial/nfc/addon/package-summary.html", type:"package" },
+      { id:11, label:"mobisocial.nfc.addon.BluetoothConnector", link:"mobisocial/nfc/addon/BluetoothConnector.html", type:"class" },
+      { id:12, label:"mobisocial.nfc.addon.BluetoothConnector.OnConnectedListener", link:"mobisocial/nfc/addon/BluetoothConnector.OnConnectedListener.html", type:"class" }
 
     ];
