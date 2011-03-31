6e1ae51635791a0cc0a6262a1cc5408a396ba775
Ben Dodson <bjdodson@handdog.(none)>
2011-03-31 09:34:40 -0700

    Added copyright notice

--- src/main/java/mobisocial/nfc/Nfc.java
+++ src/main/java/mobisocial/nfc/Nfc.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2010 Stanford University MobiSocial Lab
+ * http://mobisocial.stanford.edu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package mobisocial.nfc;
 
 import java.io.Closeable;
@@ -155,6 +172,9 @@ public class Nfc {
 		mActivity = activity;
 		try {
 			mNfcAdapter = NfcAdapter.getDefaultAdapter(mActivity);
+		} catch (NoClassDefFoundError e) {
+			Log.i(TAG, "Nfc not available.");
+			return;
 		} catch (NoSuchMethodError e) {
 			Log.i(TAG, "Nfc not available.");
 			return;
