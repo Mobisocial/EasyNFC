97dd3189ad1fed8175e61a02ae884cd04fdb25aa
Ben Dodson <bjdodson@handdog.(none)>
2011-04-04 17:42:21 -0700

    updated docs

--- apidocs/reference/lists.js
+++ apidocs/reference/lists.js
@@ -9,8 +9,9 @@ var DATA = [
       { id:7, label:"mobisocial.nfc.Nfc.NdefTcpPushHandover", link:"mobisocial/nfc/Nfc.NdefTcpPushHandover.html", type:"class" },
       { id:8, label:"mobisocial.nfc.Nfc.NfcInterface", link:"mobisocial/nfc/Nfc.NfcInterface.html", type:"class" },
       { id:9, label:"mobisocial.nfc.Nfc.OnTagWriteListener", link:"mobisocial/nfc/Nfc.OnTagWriteListener.html", type:"class" },
-      { id:10, label:"mobisocial.nfc.addon", link:"mobisocial/nfc/addon/package-summary.html", type:"package" },
-      { id:11, label:"mobisocial.nfc.addon.BluetoothConnector", link:"mobisocial/nfc/addon/BluetoothConnector.html", type:"class" },
-      { id:12, label:"mobisocial.nfc.addon.BluetoothConnector.OnConnectedListener", link:"mobisocial/nfc/addon/BluetoothConnector.OnConnectedListener.html", type:"class" }
+      { id:10, label:"mobisocial.nfc.Nfc.PrioritizedHandler", link:"mobisocial/nfc/Nfc.PrioritizedHandler.html", type:"class" },
+      { id:11, label:"mobisocial.nfc.addon", link:"mobisocial/nfc/addon/package-summary.html", type:"package" },
+      { id:12, label:"mobisocial.nfc.addon.BluetoothConnector", link:"mobisocial/nfc/addon/BluetoothConnector.html", type:"class" },
+      { id:13, label:"mobisocial.nfc.addon.BluetoothConnector.OnConnectedListener", link:"mobisocial/nfc/addon/BluetoothConnector.OnConnectedListener.html", type:"class" }
 
     ];
